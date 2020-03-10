ab59e24f1c632588a4a6b7f626ee4bc95644891d
wenbo <wenbolovesnz@gmail.com>
2020-03-11 11:38:49 +1300

    init repo with an example of upload to s3 with node js script

---
+++ app.js
@@ -0,0 +1,33 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+console.log(process.env.secretAccessKey);
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AccessKeyId,
+  secretAccessKey: process.env.SecretAccessKey
+});
+
+const uploadFile = fileName => {
+  // Read content from the file
+  const fileContent = fs.readFileSync(fileName);
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: "carters-underlying-data",
+    Key: "test.png", // File name you want to save as in S3
+    Body: fileContent
+  };
+
+  // Uploading files to the bucket
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+uploadFile("./test.png");
